d3e60fc333d775d82260743ade8af1309b56846d
JlopezV <J0274nj@gmail.com>
2023-01-28 21:11:09 -0600

    Creacion de la clase Cliente

---
+++ ED1_Practica01/Cliente1.cs
@@ -0,0 +1,13 @@
+﻿namespace ED1_Practica01
+{
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Profesion { get; set; }
+
+        public string Nacionalidad { get; set; }
+
+        public int Ingresos { get; set; }
+    }
+}
--- ED1_Practica01/Controllers/HomeController.cs
+++ ED1_Practica01/Controllers/HomeController.cs
@@ -7,15 +7,23 @@ namespace ED1_Practica01.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Cliente cliente1;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            cliente1 = new Cliente();
+
+            cliente1.Edad = 35;
+            cliente1.Nombre = "Marco";
+            cliente1.Profesion = "Arquitecto";
+            cliente1.Nacionalidad = "Guatemalteco";
+            cliente1.Ingresos = 5000;
         }
 
-        public IActionResult Index()
+        public Cliente Index()
         {
-            return View();
+            return cliente1;
         }
 
         public IActionResult Privacy()
